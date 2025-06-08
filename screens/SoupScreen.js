a74139350e7900f162e3414f221368cbe6e93c5f
ClearSkyDev <psalms638@gmail.com>
2025-06-08 16:34:51 -0400

    chore: remove unused imports in SoupScreen component

--- screens/SoupScreen.js
+++ screens/SoupScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useApp } from '../context/AppContext';
 
 export default function SoupScreen() {
